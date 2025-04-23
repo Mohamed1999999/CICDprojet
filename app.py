f3c7062382ab471d0d8b5efbea775b79851b8200
Mohamed1999999 <momog9997@gmail.com>
2025-04-23 15:53:03 +0100

    first CICDgithubactions

---
+++ app.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def main():
+    print("Hello from a Dockerized Python app!")
+    print("Current time is:", datetime.now())
+
+if __name__ == "__main__":
+    main()
